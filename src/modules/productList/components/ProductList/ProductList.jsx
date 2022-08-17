0f6e237a528576b109a6175edb6e66116996a484
Zendrian <zendrian@gmail.com>
2022-08-17 16:43:34 -0500

    Add filter

--- src/modules/productList/components/ProductList/ProductList.jsx
+++ src/modules/productList/components/ProductList/ProductList.jsx
@@ -1,17 +1,45 @@
 import TextField from "@mui/material/TextField";
-import React from "react";
+import React, { useState } from "react";
 import ProductTile from "../ProductTile/ProductTile";
 import "./ProductList.scss";
 
+function filterProducts(products, query) {
+  if (!query) return products;
+  const matchedModels = products.filter((prod) => {
+    return prod.model.toLowerCase().includes(query.toLowerCase());
+  });
+  const matchedBrands = products.filter((prod) => {
+    return prod.brand.toLowerCase().includes(query.toLowerCase());
+  });
+  const filteredRepetitions = new Set([...matchedModels, ...matchedBrands]);
+
+  return Array.from(filteredRepetitions);
+}
+
 const ProductList = ({ products }) => {
+  const [query, setQuery] = useState("");
+  const filteredProducts = filterProducts(products, query);
+
+  const handleSearchChange = (event) => {
+    setQuery(event.target.value);
+  };
+
   const generateProductTiles = () => {
-    return products.map((prod) => <ProductTile key={prod.id} product={prod} />);
+    return filteredProducts.map((prod) => (
+      <ProductTile key={prod.id} product={prod} />
+    ));
   };
 
   return (
     <section className="productList">
       <div className="productList--searchBox">
-        <TextField id="standard-basic" label="Search" variant="standard" />
+        <TextField
+          id="standard-basic"
+          label="Search"
+          variant="standard"
+          value={query}
+          onChange={handleSearchChange}
+        />
       </div>
       <div className="productList--showcase">{generateProductTiles()}</div>
     </section>
