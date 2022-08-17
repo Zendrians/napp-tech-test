017db3f021dfe6f1e660c58e8e41eb9da38e24a9
Zendrian <zendrian@gmail.com>
2022-08-17 10:36:38 -0500

    Add details route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import ProductDetailsContainer from "modules/productDetails/container/ProductDetails";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProductListContainer from "./modules/productList/container/ProductListContainer";
-// import ProductDetailsContainer from "./modules/productDetails/container/ProductDetailsContainer";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ProductListContainer />} />
-      {/* <Route path="/details" element={<ProductDetailsContainer />} /> */}
+      <Route path="/details" element={<ProductDetailsContainer />} />
     </Routes>
   );
 }
