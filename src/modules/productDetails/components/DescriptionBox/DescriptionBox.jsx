4f5b2979235d0a1fe40f74c8733e227762b4ca9b
Zendrian <zendrian@gmail.com>
2022-08-18 12:05:49 -0500

    Add product info ui

--- src/modules/productDetails/components/DescriptionBox/DescriptionBox.jsx
+++ src/modules/productDetails/components/DescriptionBox/DescriptionBox.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import MainSpecTile from "../MainSpecTile/MainSpecTile";
+import MemoryIcon from "@mui/icons-material/Memory";
+// import StorageRoundedIcon from "@mui/icons-material/StorageRounded";
+import AndroidIcon from "@mui/icons-material/Android";
+import Battery0BarIcon from "@mui/icons-material/Battery0Bar";
+import CameraAltOutlinedIcon from "@mui/icons-material/CameraAltOutlined";
+// import AspectRatioIcon from "@mui/icons-material/AspectRatio";
+// import HdOutlinedIcon from "@mui/icons-material/HdOutlined";
 import "./DescriptionBox.scss";
 
 const DescriptionBox = ({ prodDetails }) => {
+  const cameraDetails = (camera) => {
+    if (typeof camera === "string") return camera;
+    let description = "";
+    camera.forEach((element) => {
+      description += element + " ";
+    });
+    return description;
+  };
+
   return (
     <div className="descriptionBox">
       <h5 className="descriptionBox--model">{prodDetails.model}</h5>
@@ -13,10 +30,46 @@ const DescriptionBox = ({ prodDetails }) => {
       </div>
 
       <div className="descriptionBox--mainSpecsBox">
-        <div>a</div>
-        <div>b</div>
-        <div>c</div>
-        <div>d</div>
+        <MainSpecTile>
+          <MemoryIcon />
+          <span>{prodDetails.cpu}</span>
+        </MainSpecTile>
+        <MainSpecTile>
+          <CameraAltOutlinedIcon />
+          <span>{cameraDetails(prodDetails.primaryCamera)}</span>
+        </MainSpecTile>
+        <MainSpecTile>
+          <AndroidIcon />
+          <span>{prodDetails.os}</span>
+        </MainSpecTile>
+        <MainSpecTile>
+          <Battery0BarIcon />
+          <span>{prodDetails.battery}</span>
+        </MainSpecTile>
+      </div>
+      <div className="descriptionBox--secondarySpecsBox">
+        <div className="descriptionBox--secondarySpecsTile">
+          <span>RAM:</span>
+          <p>{prodDetails.ram}</p>
+        </div>
+        <div className="descriptionBox--secondarySpecsTile">
+          <span>Screen Resolution:</span>
+          <p>{prodDetails.displayResolution}</p>
+        </div>
+        <div className="descriptionBox--secondarySpecsTile">
+          <span>Dimentions:</span>
+          <p>{prodDetails.dimentions}</p>
+        </div>
+        <div className="descriptionBox--secondarySpecsTile">
+          <span>Weight:</span>
+          <p>{prodDetails.weight}</p>
+        </div>
+        {prodDetails.secondaryCmera && (
+          <div className="descriptionBox--secondarySpecsTile">
+            <span>Secondary Camera:</span>
+            <p>{cameraDetails(prodDetails.secondaryCmera)}</p>
+          </div>
+        )}
       </div>
     </div>
   );
---
+++ src/modules/productDetails/components/MainSpecTile/MainSpecTile.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MainSpecTile.scss";
+
+const MainSpecTile = ({ children }) => {
+  return <div className="mainSpecTile">{children}</div>;
+};
+
+export default MainSpecTile;
