ae0fbeebd2b9e2f1b023f05610f605d2f877754e
Zendrian <zendrian@gmail.com>
2022-08-19 10:49:13 -0500

    Add base navbar ui

--- src/shared/components/NavBar/NavBar.jsx
+++ src/shared/components/NavBar/NavBar.jsx
@@ -1,8 +1,26 @@
+import IpsumLogo from "assets/reactSvg/ipsumLogo";
 import React from "react";
-import './NavBar.scss'
+import { Link } from "react-router-dom";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import "./NavBar.scss";
 
 const NavBar = () => {
-  return <nav className="navBar">i am the navbar</nav>;
+  return (
+    <nav className="navBar">
+      <div className="navBar--navBox">
+        <Link to="/">
+          <figure>
+            <IpsumLogo />
+          </figure>
+        </Link>
+        <div className="navBar--breadCrumbsBox">Bread</div>
+        <div className="navBar--cartBox">
+          <ShoppingCartOutlinedIcon fontSize="large" style={{ color: "#ff6900" }} />
+          <span className="navBar--cartQuantity">1</span>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default NavBar;
